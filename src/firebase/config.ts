5aa1a2e7d43af0b858ee014b6225f721e3c44c34
Klein C. Balazon <klein.balazon@neu.edu.ph>
2026-03-14 09:04:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7036203810-e9405",
+  "appId": "1:315387366857:web:d2624c4f6591d9f104ac18",
+  "apiKey": "AIzaSyCwp39G2VDpSTI6Mkt46jVN4TwQdFLQkVE",
+  "authDomain": "studio-7036203810-e9405.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "315387366857"
+};
